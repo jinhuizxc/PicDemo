f5d82d2696e9c509b394fb64b7b13b3104e0b892
JinHui <hui.jin@boatchina.net>
2017-11-13 21:01:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/picdemo/bean/Bean.java
@@ -0,0 +1,46 @@
+package com.example.jh.picdemo.bean;
+
+import java.io.File;
+
+/**
+ * Created by lenovo on 2016/11/27.
+ */
+
+public class Bean {
+    private String filePath;
+    private boolean isChecked;
+    private boolean isShow;
+    private int id;
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(boolean checked) {
+        isChecked = checked;
+    }
+
+    public boolean isShow() {
+        return isShow;
+    }
+
+    public void setShow(boolean show) {
+        isShow = show;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
